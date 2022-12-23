0f35ead2eb19ff7a9418e47c8726c93f92f78d02
Tolu Joel <toluisjoel@gmail.com>
2022-12-23 15:44:56 +0100

    Added product model

--- shop/models.py
+++ shop/models.py
@@ -1,11 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.urls import reverse
 
 
 class Product(models.Model):
+    creator = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
     name = models.CharField(max_length=200)
     description = models.TextField()
     link = models.URLField()
+    slug = models.SlugField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
