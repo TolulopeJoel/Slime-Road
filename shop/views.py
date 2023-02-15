3f2f1e6430505d13967d96119ed7a8f0cb5e6719
Tolu Joel <toluisjoel@gmail.com>
2023-02-15 14:19:08 +0100

    Added alerts(messages) for form submission

--- shop/views.py
+++ shop/views.py
@@ -18,26 +18,28 @@ def creator_product_list(request, creator_id):
 def product_detail(request, id, slug, creator_id):
     creator = Creator.objects.get(id=creator_id)
     product = get_object_or_404(Product, id=id, slug=slug)
-    
+
     if request.method == 'POST':
         form = OrderCreateForm(request.POST) 
-    
+
         if form.is_valid():
             order = form.save(commit=False)
             order.product = product
             order.paid = False
-            
+
             payment_price = order.price
             product_price = product.price
 
             if product_price == 0.00:
                     form.save()
+                    messages.success(request, f'${payment_price} payment accepted')
                     # return redirect(reverse('payment:process'))
             elif payment_price >= product_price:
                     form.save()
+                    messages.success(request, f'${payment_price} payment accepted')
                     # return redirect(reverse('payment:process'))
             else:
-                messages.error(request, f'{payment_price} lower than product\'s price {product_price}')
+                messages.error(request, f'${payment_price} lower than product\'s price ${product_price}')
     else:
         form = OrderCreateForm()
 
